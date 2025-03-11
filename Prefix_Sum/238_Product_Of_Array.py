b0d9122eb0f5992de101050d2b7323d626011f6a
Callum Brezden <callumbrezden@gmail.com>
2025-03-10 19:49:01 -0700

    Solved 238

---
+++ Prefix_Sum/238_Product_Of_Array.py
@@ -0,0 +1,25 @@
+﻿from typing import List
+
+class Solution:
+    def solution(self, nums: List[int]) -> List[int]:
+        # [1,2,3,4]
+        # [1,2,6,24]
+        # [24,12,8,6]
+
+        n = len(nums)
+        prefix_product = 1
+        postfix_product = 1
+        result = [0] * n
+
+        for i in range(n):
+            result[i] = prefix_product
+            prefix_product *= nums[i]
+
+        for i in range(n - 1, -1, -1):
+            result[i] *= postfix_product
+            postfix_product *= nums[i]
+
+        return result
+
+solution = Solution()
+print(solution.solution([1,2,3,4]))
